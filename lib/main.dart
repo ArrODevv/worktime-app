4f90f5dbff14cb20ad4b25f90e1f141014685d8b
ArrODevv <dwayne.mueller.12@gmail.com>
2025-01-13 23:02:40 +0100

    Didn't change much I'm f***ing tired

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,15 @@ import 'package:worktime/pages/settings_page.dart';
 import 'package:worktime/theme_provider.dart';
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: Worktime(),
-    )
-  );
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: Worktime(),
+  ));
 }
 
 class Worktime extends StatelessWidget {
   const Worktime({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,7 +25,7 @@ class Worktime extends StatelessWidget {
 
 class MainView extends StatefulWidget {
   const MainView({super.key});
-  
+
   @override
   State<MainView> createState() => _MainViewState();
 }
@@ -35,11 +33,11 @@ class MainView extends StatefulWidget {
 class _MainViewState extends State<MainView> {
   int navigationIndex = 0;
   bool extended = false;
-  
+
   @override
   Widget build(BuildContext context) {
     Widget page;
-    switch(navigationIndex) {
+    switch (navigationIndex) {
       case 0:
         page = HomePage();
         break;
@@ -51,65 +49,64 @@ class _MainViewState extends State<MainView> {
       default:
         throw UnimplementedError('No widget for index $navigationIndex.');
     }
-    
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: extended,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.area_chart),
-                      label: Text('Analysis'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.settings),
-                      label: Text('Settings'),
-                    ),
-                  ],
-                  selectedIndex: navigationIndex,
-                  onDestinationSelected: (value) {
-                    if(navigationIndex == value) {
-                      setState(() {
-                        extended = !extended;
-                      });
-                    } else if(value == 2) {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const SettingsPage(),
-                          )
-                      );
-                    } else {
-                      setState(() {
-                        navigationIndex = value;
-                      });
-                    }
-                  },
-                ),
+
+    return Scaffold(
+        body: Row(
+      children: [
+        SafeArea(
+          child: NavigationRail(
+            extended: extended,
+            destinations: [
+              NavigationRailDestination(
+                icon: Icon(Icons.home),
+                label: Text('Home'),
               ),
-              Expanded(
-                child: Container(
-                  color: Provider.of<ThemeProvider>(context, listen: false).themeData.colorScheme.primaryContainer,
-                  child: page,
-                ),
+              NavigationRailDestination(
+                icon: Icon(Icons.area_chart),
+                label: Text('Analysis'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.settings),
+                label: Text('Settings'),
               ),
             ],
-          )
-      );
-    });
+            selectedIndex: navigationIndex,
+            onDestinationSelected: (value) {
+              if (navigationIndex == value) {
+                setState(() {
+                  extended = !extended;
+                });
+              } else if (value == 2) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsPage(),
+                    ));
+              } else {
+                setState(() {
+                  navigationIndex = value;
+                });
+              }
+            },
+          ),
+        ),
+        Expanded(
+          child: Container(
+            color: Provider.of<ThemeProvider>(context, listen: false)
+                .themeData
+                .colorScheme
+                .primaryContainer,
+            child: page,
+          ),
+        ),
+      ],
+    ));
   }
 }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-  
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -128,7 +125,7 @@ class _HomePageState extends State<HomePage> {
 
 class AnalyticsPage extends StatefulWidget {
   const AnalyticsPage({super.key});
-  
+
   @override
   State<AnalyticsPage> createState() => _AnalyticsPageState();
 }
